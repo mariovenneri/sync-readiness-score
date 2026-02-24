ac6d3976c0f5c2d545a827665a9c1d15abe04e54
mvenneri42 <mvenneri42@gmail.com>
2026-02-24 13:05:37 -0500

    AddTrack track: title

--- api/musicatlas-describe.js
+++ api/musicatlas-describe.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
         "Authorization": `Bearer ${key}`,
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ artist, track })
+      body: JSON.stringify({ artist, track: title })
     });
     const addData = await addResponse.json();
     console.log("Add track FULL response:", JSON.stringify(addData, null, 2));
